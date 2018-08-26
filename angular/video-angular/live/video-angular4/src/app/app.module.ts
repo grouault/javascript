d10023c6852ab3e012f4cccd8fb70244f2fc267e
ROUAULT Gildas <grouault@OFFICEPOMONA.FR>
2018-08-26 17:20:57 +0200

    Initalisation projet AngularVideo

---
+++ angular/video-angular/live/video-angular4/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MonComposantComponent } from './components/mon-composant/mon-composant.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MonComposantComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/video-angular/live/video-angular4/src/app/components/mon-composant/mon-composant.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+/**
+ * Created by grouault on 26/08/2018.
+ */
+
+@Component({
+  selector: 'app-mon-composant',
+  templateUrl: './mon-composant.component.html',
+  styleUrls: ['./mon-composant.component.css']
+})
+export class MonComposantComponent {
+
+  constructor(){}
+
+}
