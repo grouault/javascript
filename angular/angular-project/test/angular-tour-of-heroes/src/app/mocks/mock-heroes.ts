e849a8e430c05fd4abdeb73513db7b4e374360c3
ROUAULT Gildas <grouault@OFFICEPOMONA.FR>
2018-09-12 15:27:14 +0200

    Mock Modification

--- angular/angular-project/test/angular-tour-of-heroes/src/app/mocks/mock-heroes.ts
+++ angular/angular-project/test/angular-tour-of-heroes/src/app/mocks/mock-heroes.ts
@@ -4,14 +4,14 @@ import {Hero} from "../models/hero.model";
  */
 
 export const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {id: 11, name: 'Mr. Nice', birthday: new Date(1988, 2, 11)},
+  { id: 12, name: 'Narco', birthday: new Date(1990, 1, 16)},
+  { id: 13, name: 'Bombasto', birthday: new Date(1992, 11, 18)},
+  { id: 14, name: 'Celeritas', birthday: new Date(1975, 10, 19)},
+  { id: 15, name: 'Magneta', birthday: new Date(1979, 9, 20)},
+  { id: 16, name: 'RubberMan', birthday: new Date(1964, 8, 14)},
+  { id: 17, name: 'Dynama', birthday: new Date(1954, 7, 9)},
+  { id: 18, name: 'Dr IQ', birthday: new Date(1912, 6, 10)},
+  { id: 19, name: 'Magma', birthday: new Date(1913, 5, 4)},
+  { id: 20, name: 'Tornado', birthday: new Date(1945, 4, 5)}
 ];
